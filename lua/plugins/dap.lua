f5df9e59ad56362a657c50a981e1cfe3f3006b20
Baldebo, Emil <ebaldebo@gmail.com>
2025-04-21 17:29:09 +0200

    debugging

---
+++ lua/plugins/dap.lua
@@ -0,0 +1,52 @@
+return {
+	{
+		"mfussenegger/nvim-dap",
+		dependencies = {
+			"leoluz/nvim-dap-go",
+			"rcarriga/nvim-dap-ui",
+			"theHamsta/nvim-dap-virtual-text",
+			"nvim-neotest/nvim-nio",
+			"williamboman/mason.nvim",
+		},
+		config = function()
+			local dap = require("dap")
+			local ui = require("dapui")
+
+			require("dapui").setup()
+			require("dap-go").setup()
+
+			require("nvim-dap-virtual-text").setup()
+
+			vim.keymap.set("n", "<space>b", dap.toggle_breakpoint, { desc = "Debug Toggle [B]reakpoint" })
+			vim.keymap.set("n", "<space>gb", dap.run_to_cursor, { desc = "Debug go to cursor" })
+
+			-- Eval var under cursor
+			vim.keymap.set("n", "<space>v", function()
+				require("dapui").eval(nil, { enter = true })
+			end, { desc = "Debug evaluate" })
+
+			vim.keymap.set("n", "<F5>", dap.continue, { desc = "Debug Start/Continue" })
+			vim.keymap.set("n", "<S-F5>", function()
+				dap.terminate()
+				ui.close()
+			end, { desc = "Debug Stop" })
+			vim.keymap.set("n", "<F11>", dap.step_into, { desc = "Debug Step into" })
+			vim.keymap.set("n", "<S-F11>", dap.step_out, { desc = "Debug Step out" })
+			vim.keymap.set("n", "<F10>", dap.step_over, { desc = "Debug Step over" })
+			vim.keymap.set("n", "<F13>", dap.restart, { desc = "Debug Restart" })
+
+			dap.listeners.before.attach.dapui_config = function()
+				ui.open()
+			end
+			dap.listeners.before.launch.dapui_config = function()
+				ui.open()
+			end
+			dap.listeners.before.event_terminated.dapui_config = function()
+				ui.close()
+			end
+			dap.listeners.before.event_exited.dapui_config = function()
+				ui.close()
+			end
+		end,
+	},
+}
