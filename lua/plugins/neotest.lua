c6f3510f80f1ff20342639c499552be5e98c5bba
Baldebo, Emil <ebaldebo@gmail.com>
2025-04-23 09:59:31 +0200

    removed vim-sleuth and project, added neotest and guess-indent

---
+++ lua/plugins/neotest.lua
@@ -0,0 +1,37 @@
+return {
+	"nvim-neotest/neotest",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-neotest/nvim-nio",
+		"antoinemadec/FixCursorHold.nvim",
+		"nvim-treesitter/nvim-treesitter",
+		{ "fredrikaverpil/neotest-golang", version = "*" },
+	},
+	keys = {
+		{
+			"<leader>tt",
+			function()
+				require("neotest").run.run()
+			end,
+			desc = "[T]est Nearest",
+		},
+		{
+			"<leader>to",
+			function()
+				require("neotest").output.open({ enter = true })
+			end,
+			desc = "[T]est [O]utput",
+		},
+	},
+	config = function()
+		require("neotest").setup({
+			adapters = {
+				require("neotest-golang"),
+			},
+			diagnostic = {
+				enabled = true,
+				prefix = "TEST: ",
+			},
+		})
+	end,
+}
