4bbf640eb18ec48302192ba982463cc584909c69
Baldebo, Emil <ebaldebo@gmail.com>
2025-05-10 21:32:01 +0200

    avante

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -29,9 +29,29 @@ return {
 				map("gW", require("fzf-lua").lsp_live_workspace_symbols, "[W]orkspace Symbols")
 				map("grt", require("fzf-lua").lsp_typedefs, "[T]ype Definition")
 
+				-- This function resolves a difference between neovim nightly (version 0.11) and stable (version 0.10)
+				---@param client vim.lsp.Client
+				---@param method vim.lsp.protocol.Method
+				---@param bufnr? integer some lsp support methods only in specific files
+				---@return boolean
+				local function client_supports_method(client, method, bufnr)
+					if vim.fn.has("nvim-0.11") == 1 then
+						return client:supports_method(method, bufnr)
+					else
+						return client.supports_method(method, { bufnr = bufnr })
+					end
+				end
+
 				-- When you move your cursor, the highlights will be cleared (the second autocommand).
 				local client = vim.lsp.get_client_by_id(event.data.client_id)
-				if client then
+				if
+					client
+					and client_supports_method(
+						client,
+						vim.lsp.protocol.Methods.textDocument_documentHighlight,
+						event.buf
+					)
+				then
 					local highlight_augroup = vim.api.nvim_create_augroup("kickstart-lsp-highlight", { clear = false })
 					vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
 						buffer = event.buf,
@@ -55,14 +75,16 @@ return {
 				end
 
 				-- Inlay Hints
-				if client then
+				if
+					client
+					and client_supports_method(client, vim.lsp.protocol.Methods.textDocument_inlayHint, event.buf)
+				then
 					map("<leader>th", function()
 						vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled({ bufnr = event.buf }))
 					end, "[T]oggle Inlay [H]ints")
 				end
 			end,
 		})
-
 		-- Diagnostic Config
 		-- See :help vim.diagnostic.Opts
 		vim.diagnostic.config({
@@ -101,7 +123,9 @@ return {
 
 		-- Enable the following language servers
 		local servers = {
-			bashls = {},
+			bashls = {
+				filetypes = { "sh", "zsh" },
+			},
 			marksman = {},
 			gopls = {},
 			golangci_lint_ls = {},
@@ -116,11 +140,11 @@ return {
 		-- for you, so that they are available from within Neovim.
 		local ensure_installed = vim.tbl_keys(servers or {})
 		vim.list_extend(ensure_installed, {
+			"bashls", -- bash language server
 			"stylua", -- Used to format Lua code
 			"gofumpt", -- stricter gofmt
 			"goimports", -- import formatter
 			"golangci-lint", -- go linter
-			"bashls", -- bash language server
 			"marksman", -- markdown language server
 			"gopls", -- go language server
 			"rust_analyzer", -- rust language server
