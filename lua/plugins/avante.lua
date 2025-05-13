f8fd7b4c9352a386f249e4f9dea05af30e9cefcf
Baldebo, Emil <ebaldebo@gmail.com>
2025-05-13 08:09:51 +0200

    remove unused deps in avante

--- lua/plugins/avante.lua
+++ lua/plugins/avante.lua
@@ -20,26 +20,10 @@ return {
 		"stevearc/dressing.nvim",
 		"nvim-lua/plenary.nvim",
 		"MunifTanjim/nui.nvim",
-		"echasnovski/mini.pick",
-		"nvim-telescope/telescope.nvim",
-		"hrsh7th/nvim-cmp",
+		-- "echasnovski/mini.pick",
 		"ibhagwan/fzf-lua",
 		"nvim-tree/nvim-web-devicons",
 		"zbirenbaum/copilot.lua",
-		{
-			"HakonHarnes/img-clip.nvim",
-			event = "VeryLazy",
-			opts = {
-				default = {
-					embed_image_as_base64 = false,
-					prompt_for_file_name = false,
-					drag_and_drop = {
-						insert_mode = true,
-					},
-					use_absolute_path = true,
-				},
-			},
-		},
 		{
 			"MeanderingProgrammer/render-markdown.nvim",
 			opts = {
