de4bd16cfe5c0567390663e07db2623c7a4af548
Baldebo, Emil <ebaldebo@gmail.com>
2025-08-04 21:20:42 +0200

    nvim-treesitter native pm

---
+++ lua/pluginstmp/nvim-treesitter.lua
@@ -0,0 +1,65 @@
+vim.pack.add({
+	{
+		src = "https://github.com/nvim-treesitter/nvim-treesitter",
+		version = "master",
+	},
+	{
+		src = "http://github.com/nvim-treesitter/nvim-treesitter-textobjects",
+		version = "master",
+	},
+})
+
+require("nvim-treesitter.configs").setup({
+	auto_install = true,
+	sync_install = false,
+	highlight = {
+		enable = true,
+		additional_vim_regex_highlighting = false,
+	},
+	indent = {
+		enable = true,
+	},
+	incremental_selection = {
+		init_selection = "<Enter>",
+		node_incremental = "<Enter>",
+		scope_incremental = false,
+		node_decremental = "<Backspace>",
+	},
+
+	textobjects = {
+		select = {
+			enable = true,
+			lookahead = true,
+
+			keymaps = {
+				["af"] = "@function.outer",
+				["if"] = "@function.inner",
+				["ac"] = "@class.outer",
+				["ao"] = "@comment.outer",
+			},
+			selection_modes = {
+				["@parameter.outer"] = "v", -- charwise
+				["@function.outer"] = "V", -- linewise
+				["@class.outer"] = "<c-v>", -- blockwise
+			},
+			include_surrounding_whitespace = true,
+		},
+	},
+})
+
+vim.api.nvim_create_autocmd("PackChanged", {
+	desc = "Handle nvim-treesitter updates",
+	group = vim.api.nvim_create_augroup("nvim-treesitter-pack-changed-update-handler", { clear = true }),
+	callback = function(event)
+		if event.data.kind == "update" then
+			vim.notify("nvim-treesitter updated, running TSUpdate...", vim.log.levels.INFO)
+			---@diagnostic disable-next-line: param-type-mismatch
+			local ok = pcall(vim.cmd, "TSUpdate")
+			if ok then
+				vim.notify("TSUpdate completed successfully!", vim.log.levels.INFO)
+			else
+				vim.notify("TSUpdate command not available yet, skipping", vim.log.levels.WARN)
+			end
+		end
+	end,
+})
