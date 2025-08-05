315f171dbd2c4138145be5285f3529a322b78482
Baldebo, Emil <ebaldebo@gmail.com>
2025-08-05 09:05:35 +0200

    conform native package manager

---
+++ lua/pluginstmp/conform.lua
@@ -0,0 +1,19 @@
+vim.opt.packpath:prepend(vim.fn.stdpath("data") .. "/site")
+vim.pack.add({
+	"https://github.com/stevearc/conform.nvim",
+})
+
+require("conform").setup({
+	formatters_by_ft = {
+		lua = { "stylua" },
+		go = { "gofumpt", "goimports" },
+		nix = { "nixfmt" },
+		-- ansible = { "ansible-lint" },
+		-- rust = { "rustfmt" },
+		-- cpp = { "clang_format" },
+	},
+	format_on_save = {
+		timeout_ms = 3000,
+		lsp_format = "fallback",
+	},
+})
