3af2537673df3e5621ad5823872c43e64dcbe9c9
Baldebo, Emil <ebaldebo@gmail.com>
2026-04-02 10:35:47 +0200

    treesitter 0.12 fixes

--- init.lua
+++ init.lua
@@ -3,6 +3,27 @@ require("config.globals")
 require("config.lsp")
 require("config.keymaps")
 require("config.autocmds")
+
+-- Plugin hooks (must be before any vim.pack.add() for bootstrap)
+vim.api.nvim_create_autocmd("PackChanged", {
+	desc = "Update parsers when nvim-treesitter is updated",
+	group = vim.api.nvim_create_augroup("nvim-treesitter-pack-changed-update-handler", { clear = true }),
+	callback = function(event)
+		if event.data.spec.name == "nvim-treesitter" and event.data.kind == "update" then
+			if not event.data.active then
+				vim.cmd.packadd("nvim-treesitter")
+			end
+			vim.notify("nvim-treesitter updated, running TSUpdate...", vim.log.levels.INFO)
+			local ok = pcall(vim.cmd, "TSUpdate")
+			if ok then
+				vim.notify("TSUpdate completed successfully!", vim.log.levels.INFO)
+			else
+				vim.notify("TSUpdate command not available yet, skipping", vim.log.levels.WARN)
+			end
+		end
+	end,
+})
+
 -- Plugins --
 require("plugins.plugin-view")
 require("plugins.catppuccin")
--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -1,65 +1,65 @@
 vim.pack.add({
 	{
 		src = "https://github.com/nvim-treesitter/nvim-treesitter",
-		version = "master",
+		version = "main",
 	},
 	{
 		src = "https://github.com/nvim-treesitter/nvim-treesitter-textobjects",
-		version = "master",
+		version = "main",
 	},
 })
 
-require("nvim-treesitter.configs").setup({
-	auto_install = true,
-	sync_install = false,
-	highlight = {
-		enable = true,
-		additional_vim_regex_highlighting = false,
-	},
-	indent = {
-		enable = true,
-	},
-	incremental_selection = {
-		init_selection = "<Enter>",
-		node_incremental = "<Enter>",
-		scope_incremental = false,
-		node_decremental = "<Backspace>",
-	},
+require("nvim-treesitter").setup({})
 
-	textobjects = {
-		select = {
-			enable = true,
-			lookahead = true,
-
-			keymaps = {
-				["af"] = "@function.outer",
-				["if"] = "@function.inner",
-				["ac"] = "@class.outer",
-				["ao"] = "@comment.outer",
-			},
-			selection_modes = {
-				["@parameter.outer"] = "v", -- charwise
-				["@function.outer"] = "V", -- linewise
-				["@class.outer"] = "<c-v>", -- blockwise
-			},
-			include_surrounding_whitespace = true,
-		},
-	},
-})
-
-vim.api.nvim_create_autocmd("PackChanged", {
-	desc = "Handle nvim-treesitter updates",
-	group = vim.api.nvim_create_augroup("nvim-treesitter-pack-changed-update-handler", { clear = true }),
-	callback = function(event)
-		if event.data.kind == "update" then
-			vim.notify("nvim-treesitter updated, running TSUpdate...", vim.log.levels.INFO)
-			---@diagnostic disable-next-line: param-type-mismatch
-			local ok = pcall(vim.cmd, "TSUpdate")
-			if ok then
-				vim.notify("TSUpdate completed successfully!", vim.log.levels.INFO)
-			else
-				vim.notify("TSUpdate command not available yet, skipping", vim.log.levels.WARN)
-			end
+-- Auto-install parsers when opening a file with no parser
+vim.api.nvim_create_autocmd("FileType", {
+	group = vim.api.nvim_create_augroup("treesitter-auto-install", { clear = true }),
+	callback = function()
+		local ft = vim.bo.filetype
+		if ft == "" or not require("nvim-treesitter.parsers")[ft] then
+			return
+		end
+		if not pcall(vim.treesitter.language.inspect, ft) then
+			pcall(require("nvim-treesitter").install, { ft })
 		end
 	end,
 })
+
+-- Enable treesitter highlighting and indentation for all supported filetypes
+vim.api.nvim_create_autocmd("FileType", {
+	group = vim.api.nvim_create_augroup("treesitter-features", { clear = true }),
+	callback = function()
+		pcall(vim.treesitter.start)
+		if pcall(vim.treesitter.language.inspect, vim.bo.filetype) then
+			vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+		end
+	end,
+})
+
+-- Textobjects
+require("nvim-treesitter-textobjects").setup({
+	select = {
+		lookahead = true,
+		selection_modes = {
+			["@parameter.outer"] = "v",
+			["@function.outer"] = "V",
+			["@class.outer"] = "<c-v>",
+		},
+		include_surrounding_whitespace = true,
+	},
+})
+
+vim.keymap.set({ "x", "o" }, "af", function()
+	require("nvim-treesitter-textobjects.select").select_textobject("@function.outer", "textobjects")
+end, { desc = "Select around function" })
+vim.keymap.set({ "x", "o" }, "if", function()
+	require("nvim-treesitter-textobjects.select").select_textobject("@function.inner", "textobjects")
+end, { desc = "Select inside function" })
+vim.keymap.set({ "x", "o" }, "ac", function()
+	require("nvim-treesitter-textobjects.select").select_textobject("@class.outer", "textobjects")
+end, { desc = "Select around class" })
+vim.keymap.set({ "x", "o" }, "ao", function()
+	require("nvim-treesitter-textobjects.select").select_textobject("@comment.outer", "textobjects")
+end, { desc = "Select around comment" })
+
+
